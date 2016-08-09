7c340d881b056711fc0dea882fdbf6015facd114
Sukhbir Singh <sukhbirbedi03@gmail.com>
2016-08-09 23:08:23 +0530

    Add files via upload

---
+++ Resources/js/script.js
@@ -0,0 +1,63 @@
+$('.js--section-features').waypoint(function(direction){
+	if(direction == "down"){
+		$('nav').addClass('sticky');
+	}else{
+		$('nav').removeClass('sticky');
+	}
+},{
+	offset:'60px;'
+});
+
+// ========================================================================================
+$(".js--scroll-features").click(function(){
+	$("html,body").animate({scrollTop:$(".js--section-features").offset().top},1000);
+});
+
+$(".js--scroll-plans").click(function(){
+	$("html,body").animate({scrollTop:$(".js--section-plan").offset().top},1000);
+});
+
+$(".js--scroll-plan").click(function(){
+	$("html,body").animate({scrollTop:$(".js--section-plan").offset().top},1000);
+});
+
+$(".js--scroll-feature").click(function(){
+	$("html,body").animate({scrollTop:$(".js--section-features").offset().top},1000);
+});
+
+$(".js--scroll-city").click(function(){
+	$("html,body").animate({scrollTop:$(".js--section-city").offset().top},1000);
+});
+
+$(".js--scroll-work").click(function(){
+	$("html,body").animate({scrollTop:$(".js--section-work").offset().top},1000);
+});
+
+// ====================================================================================
+
+$(".js--wp-1").waypoint(function(direction){
+	$('.js--wp-1').addClass("animated fadeIn");
+},{
+	offset:'50%;'
+});
+
+
+$(".js--wp-2").waypoint(function(direction){
+	$(".js--wp-2").addClass("animated fadeInUp");
+},{
+	offset:'50%;'
+});
+
+
+$(".js--wp-3").waypoint(function(direction){
+	$(".js--wp-3").addClass("animated fadeIn");
+},{
+	offset:'50%;'
+});
+
+
+$(".js--wp-4").waypoint(function(direction){
+	$(".js--wp-4").addClass("animated pulse");
+},{
+	offset:'50%;'
+});
